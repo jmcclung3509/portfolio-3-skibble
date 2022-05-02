261c7ae98b66a3d4e0d81f6246b3c8aa86eba1bd
Jessica Turner <jmcclung3509@gmail.com>
2022-05-02 12:22:33 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showNav() {
+    let hamburgerIcon = document.getElementById("myLinks");
+    if (hamburgerIcon.style.display === "block") {
+        hamburgerIcon.style.display = "none";
+    } else {
+        hamburgerIcon.style.display = "block";
+    }
+}
